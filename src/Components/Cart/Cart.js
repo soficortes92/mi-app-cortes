faaced33c67135795343cc528c45f3a3074d60fd
Sofia Cortes <soficortes@live.com.ar>
2022-07-21 16:32:47 -0300

    se hacen cambios generales

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,16 @@
 import React, { useContext } from 'react';
 import { CartContext } from '../../Context/CartContext';
+import { Link } from 'react-router-dom';
 
 const Cart = () => {
-  const { cart } = useContext(CartContext);
+  const { cart, removeItem } = useContext(CartContext);
   if (cart.length === 0) {
-    return <h1>El carrito esta vacio</h1>;
+    return (
+      <>
+        <h1>El carrito esta vacio</h1>
+        <Link to="/">Volver al Inicio</Link>
+      </>
+    );
   }
   return (
     <>
@@ -13,6 +19,13 @@ const Cart = () => {
         <div key={key}>
           <div>{product.titulo}</div>
           <p>Cantidad: {product.qty}</p>
+          <button
+            onClick={() => {
+              removeItem(product.id);
+            }}
+          >
+            ELIMINAR
+          </button>
         </div>
       ))}
     </>
