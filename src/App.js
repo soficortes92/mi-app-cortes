f248e765ddbe6594fd2fdc7df14161bddb633108
Sofia Cortes <soficortes@live.com.ar>
2022-08-03 22:07:20 -0300

    cambios en librerias

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 // import logo from './logo.svg';
 // import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './Components/Navbar/NavBar';
 import ListContainer from './Components/ItemListContainer/ListContainer';
 import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,36 +1,184 @@
-import "./NavBar.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import Carrito from "../CartWidget/CartWidget";
-import { Link } from "react-router-dom";
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
-function NavBar() {
+const pages = ['Productos', 'Juegos', 'Tecnología', 'Termos'];
+const settings = ['Perfil', 'Logout'];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
-    <Navbar expand="lg" bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand href="">
-          <Link to={"/"}>LDZ.IMPORTADOS</Link>
-        </Navbar.Brand>
-        <Nav className="me-auto">
-          <Link to={"/productos"}>Productos</Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Link to={"/category/juegos"}>Juegos</Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Link to={"/category/tecnologia"}>Tecnologia</Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Link to={"/category/termos"}>Termos</Link>
-        </Nav>
-        <Nav className="me-auto">
-          <Link to={"/contacto"}>Contacto</Link>
-        </Nav>
-        <Carrito />
-      </Container>
-    </Navbar>
-  );
-}
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="/"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LDZ.Importados
+          </Typography>
 
-export default NavBar;
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: 'block', md: 'none' },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} />
+          <Typography
+            variant="h5"
+            noWrap
+            component="a"
+            href=""
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'none' },
+              flexGrow: 1,
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+            <ShoppingCartIcon fontSize='large'/>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
+
+
+
+
+// import "./NavBar.css";
+// import Carrito from "../CartWidget/CartWidget";
+// import { Link } from "react-router-dom";
+
+// function NavBar() {
+//   return (
+//   <>
+//     <Link to={"/"}>LDZ.IMPORTADOS</Link>
+//     <Link to={"/productos"}>Productos</Link>
+//     <Link to={"/category/juegos"}>Juegos</Link>
+//     <Link to={"/category/tecnologia"}>Tecnologia</Link>
+//     <Link to={"/category/termos"}>Termos</Link>
+//     <Link to={"/contacto"}>Contacto</Link>
+//     <Carrito />
+//   </>   
+//   );
+// }
+
+// export default NavBar;
